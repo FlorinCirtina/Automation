c6745fece0ec924aaf6ccca563b17c184f4a1a52
Florin Cirtina <florin.cirtina@gmail.com>
2022-02-23 16:27:38 +0200

    Some update

---
+++ src/test/java/pages/ExerciseForTesting.java
@@ -0,0 +1,62 @@
+package pages;
+
+public class ExerciseForTesting {
+
+    public static void main(String[] args) {
+//        System.out.println("Dupa tiparire trece pe rand nou");
+//        System.out.print("Dupa tiparire nu trece pe rand nou");
+//        System.out.printf("Nu trece si se foloseste pentru afisare speciala a nr");
+
+//        float num1 = (float) (1.42222*234.56433);
+//        double num2 =1.42222*234.56433;
+//
+//        System.out.printf("%f", num1);
+//        System.out.println("");
+//        System.out.printf("%.10f", num2);
+
+//        System.out.print("Tipareste 1" + " ");
+//        System.out.print("Tipareste 2");
+
+//        int anul_nasterii = 1997;
+//        System.out.println(anul_nasterii);
+//        String _23nume;
+//        anul_nasterii = 1993;
+//        System.out.println(anul_nasterii);
+//
+//        int a, b, c;
+//        a=6;
+//        b=5;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(a+b);
+//
+//        System.out.println(a!=6);
+//        String x="0dfg34ghjhj77";
+//        System.out.println(Math.abs(Integer.parseInt(x)));
+//
+//
+//        String x="Ana";
+//        String y="mere";
+//        String z=x+" are "+y;
+//        System.out.println(z);
+//
+//        int[] tablou = {1,2,3,4};
+//        System.out.println(tablou.length);
+//
+//        System.out.println();
+
+//        int x = 5;
+//        if (x == 0) {
+//            System.out.println("„Numarul este zero”");
+//        } else if (x < 0) {
+//            System.out.println("„Numarul este negativ„");
+//        } else{
+//            System.out.println("„Numarul este pozitiv„");
+//        }
+
+
+    }
+}
+
+
